0b3cdaa7dcf590454d659d881770598877dd3219
Hiếu <minhhieuqb467@gmail.com>
2023-06-29 17:53:42 +0700

    Add basic UI

---
+++ src/component/entityList.js
@@ -0,0 +1,22 @@
+export const entityList = [
+  {
+    label: "PERSON",
+    color: "#c59ff5",
+    desc: "Person",
+  },
+  {
+    label: "ORG",
+    color: "#43c6fc",
+    desc: "Organization",
+  },
+  {
+    label: "LANGUAGE",
+    color: "#a0e419",
+    desc: "Language",
+  },
+  {
+    label: "LOC",
+    color: "#ffcc00",
+    desc: "Location",
+  },
+];
